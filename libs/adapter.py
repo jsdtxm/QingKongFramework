e8df866e3e81f98f19cb51c6f69616101f756c4b
xiamin <jsdtxm@gmail.com>
2024-11-08 15:36:09 +0800

    update adapter

--- libs/adapter.py
+++ libs/adapter.py
@@ -1,27 +1,40 @@
-from core.settings import Settings
 import re
+from importlib.util import find_spec
+
+from core.settings import Settings
 
 
 def config_to_tortoise(db_config: Settings):
     """
     Convert django database config to tortoise orm config
     """
+
     tortoise_config = {
         "connections": {},
-        'apps': {
-            'main': {
-                'models': ['apps.main.models'],
-                'default_connection': 'default',
-            }
-        },
+        "apps": {},
     }
+
+    for app in Settings.INSTALLED_APPS:
+        if "." not in app:
+            app = f"apps.{app}"
+
+        if not find_spec(app):
+            raise Exception("app not found: %s", app)
+
+        if find_spec(f"{app}.models"):
+            tortoise_config["apps"][app] = {
+                "models": [app + ".models"],
+                "default_connection": "default",
+            }
+
     for alias, config in db_config.DATABASES.items():
-        if re.search(r'sqlite3?$', config["ENGINE"]):
+        if re.search(r"sqlite3?$", config["ENGINE"]):
             c = {
                 "engine": "tortoise.backends.sqlite",
                 "credentials": {
                     "file_path": str(config["NAME"]),
-                    'journal_mode': 'WAL', 'journal_size_limit': 16384, 
+                    "journal_mode": "WAL",
+                    "journal_size_limit": 16384,
                 },
             }
         else:
@@ -33,7 +46,7 @@ def config_to_tortoise(db_config: Settings):
                 "engine": "tortoise.backends." + config["ENGINE"].split(".")[-1],
                 "credentials": credentials,
             }
-        
+
         tortoise_config["connections"][alias] = c
 
     return tortoise_config
