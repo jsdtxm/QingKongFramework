a4b09112b2814db75a6b69792cdcf571e7ad9f01
xiamin <jsdtxm@gmail.com>
2024-11-11 13:46:33 +0800

    update BaseSettings

--- libs/settings.py
+++ libs/settings.py
@@ -1,6 +1,13 @@
+from pathlib import Path
+from typing import Any, List
+
 from pydantic_settings import BaseSettings as PydanticBaseSettings
 from pydantic_settings import SettingsConfigDict
 
 
 class BaseSettings(PydanticBaseSettings):
     model_config = SettingsConfigDict(env_prefix="QK_")
+
+    BASE_DIR: Path
+    INSTALLED_APPS: List[str]
+    DATABASES: dict[str, dict[str, Any]]
