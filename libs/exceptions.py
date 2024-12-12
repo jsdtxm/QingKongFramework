1de2202210f7dd498918d5c534fce6bf5fe74225
xiamin <min.xia@woshikeji.net>
2024-12-12 10:39:10 +0800

    exceptions

--- libs/exceptions.py
+++ libs/exceptions.py
@@ -1,7 +1,11 @@
+from starlette.exceptions import HTTPException
+
+
 class ImproperlyConfigured(Exception):
     """Django is somehow improperly configured"""
 
     pass
+
 
 class PermissionDenied(Exception):
     """The user did not have permission to do that"""
