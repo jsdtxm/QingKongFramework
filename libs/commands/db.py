c5745b68ce0dd16ff6769f8abc74290796d3e722
xiamin <jsdtxm@gmail.com>
2024-12-16 10:14:00 +0800

    migrate

--- libs/commands/db.py
+++ libs/commands/db.py
@@ -1,15 +1,21 @@
+import click
 import uvloop
+
 from common.settings import settings
+from libs.db.utils import generate_schemas
 from libs.initialize.apps import init_apps
 from libs.initialize.db import async_init_db, get_tortoise_config
 from libs.models.tortoise import Tortoise
 
 
-async def async_migrate():
+async def async_migrate(safe, guided):
     init_apps(settings.INSTALLED_APPS)
     await async_init_db(get_tortoise_config(settings.DATABASES))
-    await Tortoise.generate_schemas()
+    await generate_schemas(Tortoise, safe=safe, guided=guided)
     await Tortoise.close_connections()
 
-def migrate():
-    uvloop.run(async_migrate())
+
+@click.option("--safe", default=True)
+@click.option("--guided", default=True)
+def migrate(safe=True, guided=True):
+    uvloop.run(async_migrate(safe, guided))
---
+++ libs/db/utils.py
@@ -0,0 +1,116 @@
+from itertools import chain
+from typing import TYPE_CHECKING, List, Set, Type
+
+from tortoise import Model, Tortoise
+from tortoise.connection import connections
+from tortoise.exceptions import ConfigurationError
+
+if TYPE_CHECKING:  # pragma: nocoverage
+    from tortoise.backends.base.client import BaseDBAsyncClient
+
+
+def get_create_schema_sql(self, safe: bool = True) -> str:
+    models_to_create: "List[Type[Model]]" = []
+
+    self._get_models_to_create(models_to_create)
+
+    tables_to_create = []
+    for model in models_to_create:
+        tables_to_create.append(self._get_table_sql(model, safe))
+
+    tables_to_create_count = len(tables_to_create)
+
+    created_tables: Set[dict] = set()
+    ordered_tables_for_create: List[str] = []
+    m2m_tables_to_create: List[str] = []
+    while True:
+        if len(created_tables) == tables_to_create_count:
+            break
+        try:
+            next_table_for_create = next(
+                t
+                for t in tables_to_create
+                if t["references"].issubset(created_tables | {t["table"]})
+            )
+        except StopIteration:
+            raise ConfigurationError("Can't create schema due to cyclic fk references")
+        tables_to_create.remove(next_table_for_create)
+        created_tables.add(next_table_for_create["table"])
+        ordered_tables_for_create.append(next_table_for_create["table_creation_string"])
+        m2m_tables_to_create += next_table_for_create["m2m_tables"]
+
+    return chain(ordered_tables_for_create + m2m_tables_to_create)
+
+
+def get_schema_sql(client: "BaseDBAsyncClient", safe: bool) -> str:
+    """
+    Generates the SQL schema for the given client.
+
+    :param client: The DB client to generate Schema SQL for
+    :param safe: When set to true, creates the table only when it does not already exist.
+    """
+    generator = client.schema_generator(client)
+    return get_create_schema_sql(generator, safe)
+
+
+async def generate_schema_for_client(
+    client: "BaseDBAsyncClient", safe: bool, guided: bool
+) -> None:
+    """
+    Generates and applies the SQL schema directly to the given client.
+
+    :param client: The DB client to generate Schema SQL for
+    :param safe: When set to true, creates the table only when it does not already exist.
+    """
+    generator = client.schema_generator(client)
+    schema_list = get_schema_sql(client, safe)
+    for schema in schema_list:
+        if not schema:  # pragma: nobranch
+            continue
+
+        print(schema)
+        user_input = "Y"
+        try:
+            while guided:
+                user_input = (
+                    input(
+                        "Please enter '[Y]' to execute the sql, 'N' to skip the sql, or 'Q' to quit: "
+                    )
+                    .strip()
+                    .upper()
+                )
+                if user_input == "":
+                    user_input = "Y"
+                    break
+                elif user_input not in ("Y", "N", "Q"):
+                    print(f"Invalid input '{user_input}'")
+                    continue
+                else:
+                    break
+        except KeyboardInterrupt:
+            return
+
+        if user_input == "Y":
+            await generator.generate_from_string(schema)
+        elif user_input == "N":
+            continue
+        elif user_input == "Q":
+            return
+
+
+async def generate_schemas(cls: Tortoise, safe: bool = True, guided=False) -> None:
+    """
+    Generate schemas according to models provided to ``.init()`` method.
+    Will fail if schemas already exists, so it's not recommended to be used as part
+    of application workflow
+
+    :param safe: When set to true, creates the table only when it does not already exist.
+
+    :raises ConfigurationError: When ``.init()`` has not been called.
+    """
+    if not cls._inited:
+        raise ConfigurationError(
+            "You have to call .init() first before generating schemas"
+        )
+    for connection in connections.all():
+        await generate_schema_for_client(connection, safe, guided)
