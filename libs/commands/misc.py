f3fce80ba83f1d40bdc5b98db3e70a7b53f5f966
xiamin <min.xia@woshikeji.net>
2024-12-17 11:14:01 +0800

    init_models

--- libs/commands/misc.py
+++ libs/commands/misc.py
@@ -1,5 +1,6 @@
 from common.settings import settings
 from libs.initialize.apps import init_apps
+from libs.initialize.db import init_models
 from libs.misc import model_stub
 from libs.misc.ascii_art import print_logo
 from libs.utils.module_loading import package_try_import
@@ -11,6 +12,7 @@ def about():
 
 def stubgen():
     apps = init_apps(settings.INSTALLED_APPS)
+    init_models()
     for app_config in apps.app_configs.values():
         if models := package_try_import(app_config.module, "models"):
             model_stub.generate(models.__name__)
