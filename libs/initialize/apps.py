8194dee2afcc604364b976f529f831314f8a8224
xiamin <jsdtxm@gmail.com>
2024-12-26 09:29:49 +0800

    update init_apps

--- libs/initialize/apps.py
+++ libs/initialize/apps.py
@@ -3,8 +3,9 @@ from typing import Sequence
 from libs import apps
 
 
-def init_apps(installed_apps: Sequence[str]):
-    if len(installed_apps) != 0 and len(apps.apps.installed_apps) == 0:
+def init_apps(installed_apps: Sequence[str], override: bool = False):
+    """Initialize apps"""
+    if len(installed_apps) != 0 and (override or len(apps.apps.installed_apps) == 0):
         apps.apps.populate(installed_apps)
     
     return apps.apps
