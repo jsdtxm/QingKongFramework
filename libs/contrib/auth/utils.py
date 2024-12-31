849608165cdf5c14b21e01d4aa93a0dc5aae0519
xiamin <jsdtxm@gmail.com>
2024-12-31 09:17:38 +0800

    mv get_user_model

---
+++ libs/contrib/auth/utils.py
@@ -0,0 +1,26 @@
+from typing import TYPE_CHECKING
+
+from libs.exceptions import ImproperlyConfigured
+from libs.utils.module_loading import import_string
+
+if TYPE_CHECKING:
+    from libs.contrib.auth.models import AbstractUser
+
+
+def get_user_model() -> "AbstractUser":
+    """
+    Return the User model that is active in this project.
+    """
+    from common.settings import settings
+
+    try:
+        return import_string(settings.AUTH_USER_MODEL)
+    except ValueError:
+        raise ImproperlyConfigured(
+            "AUTH_USER_MODEL must be of the form 'app_label.model_name'"
+        )
+    except LookupError:
+        raise ImproperlyConfigured(
+            "AUTH_USER_MODEL refers to model '%s' that has not been installed"
+            % settings.AUTH_USER_MODEL
+        )
