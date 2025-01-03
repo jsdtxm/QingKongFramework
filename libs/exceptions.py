6ec380cb86eb3b7930c49c5138a99ac841240063
xiamin <jsdtxm@gmail.com>
2025-01-03 10:17:10 +0800

    exceptions

--- libs/exceptions.py
+++ libs/exceptions.py
@@ -1,3 +1,5 @@
+import http
+
 from starlette.exceptions import HTTPException
 
 
@@ -11,3 +13,16 @@ class PermissionDenied(Exception):
     """The user did not have permission to do that"""
 
     pass
+
+
+class Http404(HTTPException):
+    def __init__(
+        self,
+        detail: str | None = None,
+        headers: dict[str, str] | None = None,
+    ) -> None:
+        if detail is None:
+            detail = http.HTTPStatus(404).phrase
+        self.status_code = 404
+        self.detail = detail
+        self.headers = headers
