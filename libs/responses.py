90d227f6f7ba2bc640c86350e6543255e454ba21
xiamin <min.xia@woshikeji.net>
2025-01-08 13:13:02 +0800

    FileResponse

--- libs/responses.py
+++ libs/responses.py
@@ -1,8 +1,9 @@
 import json
+import os
 import typing
-
+from io import IOBase
 from starlette.background import BackgroundTask
-from starlette.responses import FileResponse as FileResponse  # noqa
+from starlette.responses import FileResponse as StarletteFileResponse  # noqa
 from starlette.responses import HTMLResponse as HTMLResponse  # noqa
 from starlette.responses import JSONResponse as StarletteJSONResponse  # noqa
 from starlette.responses import PlainTextResponse as PlainTextResponse  # noqa
@@ -39,7 +40,15 @@ class JsonResponse(JSONResponse):
     pass
 
 
-class HttpResponse(Response):
+class ResponseHeaderOperatorsMixin:
+    def __getitem__(self, key):
+        return self.headers[key]
+
+    def __setitem__(self, key, value):
+        self.headers[key] = value
+
+
+class HttpResponse(ResponseHeaderOperatorsMixin, Response):
     def __init__(
         self,
         content: typing.Any = None,
@@ -50,14 +59,41 @@ class HttpResponse(Response):
         charset: str = "urf-8",
     ) -> None:
         self.charset = charset
-        
+
         super().__init__(content, status_code, headers, content_type, background)
 
-    def __getitem__(self, key):
-        return self.headers[key]
 
-    def __setitem__(self, key, value):
-        self.headers[key] = value
+class FileResponse(ResponseHeaderOperatorsMixin, StarletteFileResponse):
+    def __init__(
+        self,
+        path: IOBase | str | os.PathLike[str],
+        status_code: int = 200,
+        headers: typing.Mapping[str, str] | None = None,
+        media_type: str = "application/octet-stream",
+        background: BackgroundTask | None = None,
+        filename: str | None = None,
+        stat_result: os.stat_result | None = None,
+        method: str | None = None,
+        content_disposition_type: str = "attachment",
+    ) -> None:
+        if isinstance(path, IOBase):
+            path.close()
+            path = getattr(path, "name", None)
+
+            if path is None:
+                raise Exception("Not support this type")
+
+        super().__init__(
+            path,
+            status_code,
+            headers,
+            media_type,
+            background,
+            filename,
+            stat_result,
+            method,
+            content_disposition_type,
+        )
 
 
 class HttpResponseNotAllowed(HttpResponse):
