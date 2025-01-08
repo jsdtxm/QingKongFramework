9efea9607ec2671d4c6be797bae9f5a9618307de
xiamin <min.xia@woshikeji.net>
2025-01-08 12:47:39 +0800

    handle files

---
+++ libs/datastructures.py
@@ -0,0 +1,36 @@
+import typing
+
+from fastapi.datastructures import UploadFile as FastApiUploadFile
+from starlette.datastructures import FormData
+from starlette.datastructures import Headers as Headers  # noqa: F401
+from starlette.datastructures import ImmutableMultiDict
+
+
+class UploadFile(FastApiUploadFile):
+    async def chunks(self, chunk_size=None):
+        self.file.seek(0)
+        yield self.read()
+
+    @property
+    def name(self):
+        return self.filename
+
+
+class FileFormData(ImmutableMultiDict[str, UploadFile]):
+    """
+    An immutable multidict, containing both file uploads and text input.
+    """
+
+    def __init__(
+        self,
+        *args: FormData
+        | typing.Mapping[str, UploadFile]
+        | list[tuple[str, UploadFile]],
+        **kwargs: UploadFile,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+
+    async def close(self) -> None:
+        for key, value in self.multi_items():
+            if isinstance(value, UploadFile):
+                await value.close()
--- libs/requests.py
+++ libs/requests.py
@@ -1,9 +1,15 @@
 from fastapi.requests import Request as Request  # noqa
 from typing import TYPE_CHECKING, Optional
+from libs.datastructures import FileFormData, UploadFile
+from starlette.datastructures import UploadFile as StarletteUploadFile
+
 
 if TYPE_CHECKING:
     from libs.contrib.auth.typing import UserProtocol
 
+
+class Empty:
+    pass
 
 class DjangoStyleRequest:
     """keep no additional data"""
@@ -21,3 +27,19 @@ class DjangoStyleRequest:
     @property
     def POST(self):
         return self.form()
+    
+    @property
+    async def FILES(self) -> FileFormData:
+        form = {k: v for k, v in (await self.form()).items() if isinstance(v, StarletteUploadFile)}
+        for v in form.values():
+            v.__class__ = UploadFile
+
+        return FileFormData(form)
+    
+    @property
+    def method(self):
+        return self.request.method
+    
+    @property
+    def form(self):
+        return self.request.form
