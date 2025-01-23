13d2204df3a9fd6ec28532513b4430cb04220422
xiamin <jsdtxm@live.com>
2025-01-23 09:05:38 +0000

    auth mixin

--- libs/contrib/auth/mixins.py
+++ libs/contrib/auth/mixins.py
@@ -1,4 +1,7 @@
 from libs.exceptions import NotAuthenticated
+from libs.serializers import ModelSerializer
+from libs.views.viewsets import GenericViewSet
+
 
 class AccessMixin:
     """
@@ -18,6 +21,25 @@ class LoginRequiredMixin(AccessMixin):
     """Verify that the current user is authenticated."""
 
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
+        if request.user is None or not request.user.is_authenticated:
             return self.handle_no_permission()
         return super().dispatch(request, *args, **kwargs)
+
+
+class CreatorMixin:
+    creator_field = "created_by_id"
+
+    async def perform_create(self: "GenericViewSet", serializer: ModelSerializer):
+        if self.request.user:
+            setattr(serializer, self.creator_field, self.request.user.id)  # type: ignore[attr-defined]
+        else:
+            setattr(serializer, self.creator_field, None)  # type: ignore[attr-defined]
+        await serializer.save()
+
+
+class CreatorWithFilterMixin(LoginRequiredMixin, CreatorMixin):
+    
+    async def filter_queryset(self, queryset):
+        assert self.request.user
+        queryset = queryset.filter(**{self.creator_field: self.request.user.id})
+        return queryset
