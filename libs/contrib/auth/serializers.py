fdc0969f800299e3efdc369821119db03073e1a1
xiamin <jsdtxm@live.com>
2025-02-08 07:58:45 +0000

    update UserSerializer

--- libs/contrib/auth/serializers.py
+++ libs/contrib/auth/serializers.py
@@ -3,8 +3,8 @@ from libs.contrib.auth.utils import get_user_model
 
 User = get_user_model()
 
-
+# TODO 修改一下User的内置Serializer（Pydantic）排除掉password字段
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        exclude = ("password",)
