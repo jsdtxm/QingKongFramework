2897c6609f116571b2868cf2f5b95875e8d0a5e9
xiamin <jsdtxm@live.com>
2025-02-10 02:26:22 +0000

    update load_data

--- libs/commands/load_data.py
+++ libs/commands/load_data.py
@@ -35,10 +35,24 @@ def find_file_in_fixtures(folders, filename):
     return found_files
 
 
-async def _loaddata(file_path):
-    init_apps(settings.INSTALLED_APPS)
-    await async_init_db(get_tortoise_config(settings.DATABASES))
+def get_all_fixtures(folders):
+    found_files = []
 
+    for folder in folders:
+        # 构建目标子文件夹的路径
+        fixtures_path = os.path.join(folder, "fixtures")
+
+        # 检查子文件夹是否存在
+        if os.path.isdir(fixtures_path):
+            # 遍历子文件夹中的所有文件和子文件夹
+            for root, dirs, files in os.walk(fixtures_path):
+                for filename in files:
+                    if filename.endswith(".json"):
+                        found_files.append(os.path.join(root, filename))
+
+    return sorted(found_files)
+
+async def _loaddata_inner(file_path):
     if "/" not in file_path and not os.path.exists(file_path):
         app_dirs = list(
             map(
@@ -64,9 +78,27 @@ async def _loaddata(file_path):
             instance = model(id=item["pk"], **item["fields"])
             await instance.save()
 
+async def _loaddata(file_path):
+    init_apps(settings.INSTALLED_APPS)
+    await async_init_db(get_tortoise_config(settings.DATABASES))
+
+    if file_path == "all":
+        app_dirs = list(
+            map(
+                lambda x: x.replace(".", "/"),
+                filter(lambda x: x.startswith("apps"), settings.INSTALLED_APPS),
+            )
+        )
+        files = get_all_fixtures(app_dirs)
+        for file in files:
+            print(file)
+            await _loaddata_inner(file)
+    else:
+        await _loaddata_inner(file_path)
+
     await Tortoise.close_connections()
 
 
-@click.argument("file_path", type=click.STRING)
+@click.argument("file_path", type=click.STRING, default="all")
 def loaddata(file_path):
     uvloop.run(_loaddata(file_path))
