585ee95e2c039e95d790bac25c04b5e560846c67
xiamin <jsdtxm@live.com>
2025-02-11 01:55:52 +0000

    fix shell close connection

--- libs/commands/shell.py
+++ libs/commands/shell.py
@@ -5,6 +5,7 @@ from common.settings import settings
 from libs.initialize.apps import init_apps
 from libs.initialize.cache import init_cache
 from libs.initialize.db import async_init_db, get_tortoise_config
+from libs.models.tortoise import Tortoise
 
 
 def shell():
@@ -16,3 +17,5 @@ def shell():
     init_cache()
 
     code.interact(local=locals())
+
+    loop.run_until_complete(Tortoise.close_connections())
