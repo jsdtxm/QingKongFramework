78bc818085136ad3b569ac820f1b520a82a8e122
xiamin <jsdtxm@live.com>
2025-02-17 03:10:42 +0000

    add TODO

--- libs/commands/group.py
+++ libs/commands/group.py
@@ -3,5 +3,6 @@ import click
 
 class Group(click.Group):
     def register_commands(self, *commands):
+        # TODO 检查是否已经注册app
         for command in commands:
             self.command()(command)
