1bc03b217b8822807e901f590c3a8bbab9f34536
xiamin <jsdtxm@live.com>
2025-02-20 07:04:10 +0000

    fix manager

--- libs/models/info.py
+++ libs/models/info.py
@@ -3,6 +3,8 @@ from typing import TYPE_CHECKING
 from tortoise.models import MetaInfo as TortoiseMetaInfo
 from tortoise.models import Model
 
+from libs.models.base import Manager
+
 if TYPE_CHECKING:
     from libs.apps import AppConfig
 
@@ -15,3 +17,5 @@ class MetaInfo(TortoiseMetaInfo):
         self.ignore_schema: bool = getattr(meta, "ignore_schema", self.external)
         self.app_config: AppConfig = getattr(meta, "app_config", None)
         super().__init__(meta)
+        # Override manager
+        self.manager: Manager = getattr(meta, "manager", Manager())
