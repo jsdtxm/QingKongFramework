36f1939c93f98c3fb04f246a6b91ef3be6ab2904
xiamin <jsdtxm@live.com>
2025-03-13 11:34:11 +0000

    fix Model Meta type hint

--- libs/models/model.py
+++ libs/models/model.py
@@ -1,9 +1,19 @@
+from typing import TYPE_CHECKING, Any, Type
+
 from . import fields
-from .base import BaseModel, BaseMeta
+from .base import BaseMeta, BaseModel
 
 
 class Model(BaseModel):
     id = fields.BigIntegerField(primary_key=True)
 
     class Meta(BaseMeta):
+        """Model Meta"""
+
         abstract = True
+
+    if TYPE_CHECKING:
+        # 避免子类 type hint 报错
+        Meta: Type[Any]
+        PydanticMeta: Type[Any]
+        StubGenMeta: Type[Any]
