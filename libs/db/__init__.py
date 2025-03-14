a7ab3df8f636a8563ef491637b63b516532909f4
xiamin <jsdtxm@live.com>
2025-03-14 06:42:03 +0000

    BaseDBAsyncClient

--- libs/db/__init__.py
+++ libs/db/__init__.py
@@ -1,22 +1,25 @@
 from tortoise import connections as tortoise_connections
 from tortoise import transactions as transaction
+from tortoise.backends.base.client import BaseDBAsyncClient
 from tortoise.router import router
+
 from libs import models
+
 
 class ConnectionsWrapper:
     def __init__(self, connections):
         self.connections = connections
-    
-    def __getitem__(self, key):
+
+    def __getitem__(self, key) -> BaseDBAsyncClient:
         conn = self.connections.get(key)
         if conn is None:
             raise KeyError
-        
+
         return conn
-    
+
     def get(self, key):
         return self.connections.get(key)
-    
+
     def keys(self):
         return self.connections.db_config.keys()
 
