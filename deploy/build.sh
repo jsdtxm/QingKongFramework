7b4b3e9385dac2953c77374b2447f2ecf560d625
xiamin <jsdtxm@live.com>
2025-03-31 07:36:30 +0000

    update deploy

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,3 +1,1 @@
-docker build -t qingkong_framework:latest-py3.12-alpine --progress plain -f deploy/Dockerfile .
-
-docker build -t qingkong_framework:latest-py3.12-bookworm --progress plain -f deploy/Dockerfile.debian .
+docker build -t qingkong_framework:latest-py3.12 --progress plain -f deploy/Dockerfile.debian .
