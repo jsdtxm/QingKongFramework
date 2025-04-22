5490399aa04bf74ccdac4e74868f31500575baf2
xiamin <jsdtxm@live.com>
2025-04-22 08:40:08 +0000

    refactor(fs): replace fcntl with portalocker for file locking

--- fastapp/utils/fs.py
+++ fastapp/utils/fs.py
@@ -1,8 +1,8 @@
-import fcntl
 import json
 import os
 import socket
-import time
+
+import portalocker
 
 
 def write_port_to_json(
@@ -24,19 +24,13 @@ def write_port_to_json(
         with open(json_file_path, "w") as f:
             json.dump({}, f)
 
-    start_time = time.time()
     while True:
         with open(json_file_path, "r+") as f:
             try:
-                # 尝试获取锁
-                fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
-            except IOError:
-                # 如果无法立即获得锁，则检查是否超时
-                if time.time() - start_time > timeout:
-                    raise Exception("获取文件锁超时")
-                else:
-                    time.sleep(0.1)  # 等待一段时间后重试
-                    continue
+                # 尝试获取排他锁，设置超时时间
+                portalocker.lock(f, portalocker.LOCK_EX, timeout=timeout)
+            except portalocker.LockException:
+                raise Exception("获取文件锁超时")
 
             # 成功获取锁后读取现有内容
             try:
@@ -53,7 +47,7 @@ def write_port_to_json(
             f.truncate()  # 清除文件末尾可能遗留的内容
 
             # 解锁
-            fcntl.flock(f, fcntl.LOCK_UN)
+            portalocker.unlock(f)
             break
 
 
@@ -66,7 +60,6 @@ def read_port_from_json(app_name, json_file_path="service_ports.json", timeout=1
     :param timeout: 锁定文件前的最大等待时间
     :return: 包含address和port的字典，如果未找到对应的应用名，则返回None
     """
-    start_time = time.time()
     while True:
         if not os.path.exists(json_file_path):
             with open(json_file_path, "w") as f:
@@ -74,15 +67,10 @@ def read_port_from_json(app_name, json_file_path="service_ports.json", timeout=1
 
         with open(json_file_path, "r") as f:
             try:
-                # 尝试获取共享锁（允许其他读操作同时进行）
-                fcntl.flock(f, fcntl.LOCK_SH | fcntl.LOCK_NB)
-            except IOError:
-                # 如果无法立即获得锁，则检查是否超时
-                if time.time() - start_time > timeout:
-                    raise Exception("获取文件锁超时")
-                else:
-                    time.sleep(0.1)  # 等待一段时间后重试
-                    continue
+                # 尝试获取排他锁，设置超时时间
+                portalocker.lock(f, portalocker.LOCK_EX, timeout=timeout)
+            except portalocker.LockException:
+                raise Exception("获取文件锁超时")
 
             # 成功获取锁后读取现有内容
             try:
@@ -92,7 +80,7 @@ def read_port_from_json(app_name, json_file_path="service_ports.json", timeout=1
                 result = None
 
             # 解锁
-            fcntl.flock(f, fcntl.LOCK_UN)
+            portalocker.unlock(f)
             break
 
     if result is not None:
