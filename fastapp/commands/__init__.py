0c604163793623f3f685f29567fb4dc8b1d8e5a7
xiamin <jsdtxm@live.com>
2025-04-22 15:29:46 +0000

    feat(db): add auto_migrate command to db module

--- fastapp/commands/__init__.py
+++ fastapp/commands/__init__.py
@@ -1,6 +1,6 @@
 from .check import check
 from .create import startapp
-from .db import async_migrate, migrate, reverse_generation
+from .db import async_migrate, auto_migrate, migrate, reverse_generation
 from .decorators import async_init_qingkong
 from .group import Group
 from .load_data import loaddata
