d273a6e821d2786cf98d6ceefdd23621ba850553
xiamin <jsdtxm@live.com>
2025-04-27 10:13:26 +0000

    feat(db/mysql): add JSON filter support and executor enhancements

--- fastapp/db/backends/mysql.py
+++ fastapp/db/backends/mysql.py
@@ -1,15 +1,68 @@
-from tortoise.backends import mysql
-from tortoise.backends.mysql import schema_generator
+import operator
+from typing import Dict
 
-from .mixin import SchemaGeneratorMixin
+from pypika.terms import Criterion, Term
+from tortoise.backends import mysql
+from tortoise.backends.mysql import executor, schema_generator
+from tortoise.backends.mysql.executor import MySQLExecutor as RawMySQLExecutor
+from tortoise.contrib.mysql.json_functions import (
+    JSONExtract,
+    _serialize_value,
+    operator_keywords,
+)
+from tortoise.filters import (
+    contains,
+    ends_with,
+    insensitive_contains,
+    insensitive_ends_with,
+    insensitive_exact,
+    insensitive_starts_with,
+    json_contained_by,
+    json_contains,
+    json_filter,
+    posix_regex,
+    search,
+    starts_with,
+)
+
+from fastapp.db.backends.mixin import SchemaGeneratorMixin
 
 
 class MySQLSchemaGenerator(SchemaGeneratorMixin, schema_generator.MySQLSchemaGenerator):
     pass
 
 
+def mysql_json_filter(field: Term, value: Dict) -> Criterion:
+    ((key, filter_value),) = value.items()
+    filter_value = _serialize_value(filter_value)
+    key_parts = [int(item) if item.isdigit() else str(item).replace("_\\_", "__") for item in key.split("__")]
+    operator_ = operator.eq
+    if key_parts[-1] in operator_keywords:
+        operator_ = operator_keywords[str(key_parts.pop(-1))]  # type: ignore
+
+    return operator_(JSONExtract(field, key_parts), filter_value)
+
+
+class MySQLExecutor(RawMySQLExecutor):
+    FILTER_FUNC_OVERRIDE = {
+        contains: executor.mysql_contains,
+        starts_with: executor.mysql_starts_with,
+        ends_with: executor.mysql_ends_with,
+        insensitive_exact: executor.mysql_insensitive_exact,
+        insensitive_contains: executor.mysql_insensitive_contains,
+        insensitive_starts_with: executor.mysql_insensitive_starts_with,
+        insensitive_ends_with: executor.mysql_insensitive_ends_with,
+        search: executor.mysql_search,
+        json_contains: executor.mysql_json_contains,
+        json_contained_by: executor.mysql_json_contained_by,
+        json_filter: mysql_json_filter,
+        posix_regex: executor.mysql_posix_regex,
+    }
+
+
 class MySQLClient(mysql.MySQLClient):
     schema_generator = MySQLSchemaGenerator
+    executor_class = MySQLExecutor
 
 
 client_class = MySQLClient
