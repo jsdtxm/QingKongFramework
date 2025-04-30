d650093f6b2207ffcd980091a5343f46ad6f42f7
xiamin <jsdtxm@live.com>
2025-04-30 06:19:00 +0000

    fix(dynamic_rbac): correct viewset for group endpoint

--- fastapp/contrib/dynamic_rbac/urls.py
+++ fastapp/contrib/dynamic_rbac/urls.py
@@ -5,5 +5,5 @@ from fastapp.router import include, path
 urlpatterns = [
     path("", include(dynamic_rbac_router)),
     path("dynamic_permission", views.DynamicPermissionViewSet.as_view()),
-    path("group", views.DynamicPermissionViewSet.as_view()),
+    path("group", views.AdminGroupWithDynamicPermissionViewSet.as_view()),
 ]
