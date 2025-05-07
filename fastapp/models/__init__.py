a2782b59832cdb53c024f34c56b5a15ebbc881c7
xiamin <jsdtxm@live.com>
2025-05-07 07:47:15 +0000

    refactor(models): import F and Trim functions from tortoise

--- fastapp/models/__init__.py
+++ fastapp/models/__init__.py
@@ -1,13 +1,12 @@
 from pypika.functions import Cast
-from tortoise.expressions import Q, Subquery
+from tortoise.expressions import F, Q, Subquery
 from tortoise.fields.base import OnDelete
-from tortoise.functions import Count, Sum
+from tortoise.functions import Count, Sum, Trim
 from tortoise.indexes import Index
 
 from .base import BaseModel, Manager, QuerySet
 from .fields import *
 from .model import BaseMeta, Model
 from .utils import get_object_or_404
-
 
 PROTECT = OnDelete.RESTRICT
