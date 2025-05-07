53def4a140d661e62592458dfba143f83bed0e05
xiamin <jsdtxm@live.com>
2025-05-07 04:04:46 +0000

    todo: add get_client_ip todo

--- fastapp/utils/ip.py
+++ fastapp/utils/ip.py
@@ -6,5 +6,6 @@ from starlette.websockets import WebSocket
 
 def get_client_ip(request: Union[Request, WebSocket]):
     forwarded = request.headers.get("X-Forwarded-For")
-
+    # TODO 有说用REMOTE_ADDR获取的
+    # ip = request.META.get('REMOTE_ADDR')
     return forwarded.split(",")[0] if forwarded else request.client.host
