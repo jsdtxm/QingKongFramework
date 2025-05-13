107e7c1cda4341393f1faa8dc5b19ee5b13379b9
xiamin <jsdtxm@live.com>
2025-05-13 01:52:47 +0000

    fix(group): ensure commands are registered correctly

--- fastapp/commands/group.py
+++ fastapp/commands/group.py
@@ -5,4 +5,7 @@ class Group(click.Group):
     def register_commands(self, *commands):
         # TODO 检查是否已经注册app
         for command in commands:
+            if isinstance(command, click.Command):
+                self.add_command(command)
+                continue
             self.command()(command)
