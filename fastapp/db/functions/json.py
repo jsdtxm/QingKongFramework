903732ce20434ed262210883c834d7bef39ab882
xiamin <jsdtxm@live.com>
2025-05-19 15:40:21 +0800

    feat(db): add JsonExtract class for JSON path extraction

--- fastapp/db/functions/json.py
+++ fastapp/db/functions/json.py
@@ -4,3 +4,8 @@ from pypika.terms import Function
 class JsonUnquote(Function):
     def __init__(self, expr, alias=None):
         super(JsonUnquote, self).__init__("JSON_UNQUOTE", expr, alias=alias)
+
+
+class JsonExtract(Function):
+    def __init__(self, expr, path, alias=None):
+        super(JsonExtract, self).__init__("JSON_EXTRACT", expr, path, alias=alias)
