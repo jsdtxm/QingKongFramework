59361c2d8cc797f04516ba723c6c17f13335cf26
xiamin <jsdtxm@live.com>
2025-05-23 11:54:26 +0800

    feat(decorators): add cache initialization to async_init_fastapp

--- fastapp/commands/decorators.py
+++ fastapp/commands/decorators.py
@@ -2,6 +2,7 @@ import functools
 
 from common.settings import settings
 from fastapp.initialize.apps import init_apps
+from fastapp.initialize.cache import init_cache
 from fastapp.initialize.db import async_init_db, get_tortoise_config
 from fastapp.models.tortoise import Tortoise
 
@@ -10,6 +11,7 @@ def async_init_fastapp(func):
     @functools.wraps(func)  # 使用wraps以保留原始函数的元信息
     async def wrapper(*args, **kwargs):
         init_apps(settings.INSTALLED_APPS)
+        await init_cache()
         await async_init_db(get_tortoise_config(settings.DATABASES))
 
         try:
