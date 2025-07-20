292189e56e7fbdff41a4fbded62207d0ac68b123
xiamin <jsdtxm@live.com>
2025-07-20 04:06:02 +0000

    feat(model): add nested verbose name dictionary function

--- fastapp/utils/model.py
+++ fastapp/utils/model.py
@@ -59,3 +59,34 @@ def get_verbose_name_dict(fields_map: dict, extra_verbose_name_dict=None):
                     res[f"{k}.{sk}"] = f"{verbose_name}.{sub_verbose_name}"
 
     return res
+
+
+def get_verbose_name_dict_nesting(fields_map: dict, extra_verbose_name_dict=None):
+    """get_verbose_name_dict_nesting"""
+
+    res = {}
+    extra_verbose_name_dict = extra_verbose_name_dict or {}
+
+    for k, v in fields_map.items():
+        verbose_name = getattr(v, "verbose_name", None) or extra_verbose_name_dict.get(
+            k, k
+        )
+        if verbose_name and verbose_name != k:
+            res[k] = {
+                "verbose_name": verbose_name,
+            }
+            if isinstance(v, RelationalField):
+                res[f"{k}_id"] = {
+                    "verbose_name": f"{verbose_name}ID",
+                }
+                for sk, sv in v.related_model._meta.fields_map.items():
+                    sub_verbose_name = getattr(
+                        sv, "verbose_name", None
+                    ) or extra_verbose_name_dict.get(sk, sk)
+                    if "children" not in res[k]:
+                        res[k]["children"] = {}
+                    res[k]["children"][sk] = {
+                        "verbose_name": sub_verbose_name,
+                    }
+
+    return res
