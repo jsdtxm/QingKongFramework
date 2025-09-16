79dd2ea5bd5ad6e2b53f76071b50f506c9c1e23f
xiamin <jsdtxm@live.com>
2025-09-16 08:04:47 +0000

    refactor(models): reorder and add missing function imports

--- fastapp/models/__init__.py
+++ fastapp/models/__init__.py
@@ -1,14 +1,14 @@
 from pypika.functions import Cast
 from tortoise.expressions import F, Q, RawSQL, Subquery
 from tortoise.fields.base import OnDelete
-from tortoise.functions import Count, Sum, Trim
+from tortoise.functions import Count, Max, Min, Sum, Trim
 from tortoise.indexes import Index
 
 from fastapp.db.index import ExpressionIndex
 
 from .base import BaseModel
 from .fields import *
-from .functions import Instr, JsonExtract, JsonUnquote, Right, Abs, Diff
+from .functions import Abs, Diff, Instr, JsonExtract, JsonUnquote, Right
 from .manager import Manager
 from .model import BaseMeta, Model
 from .queryset import QuerySet
