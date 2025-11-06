d5e69547acbd80586399c9afbc46c6626df340fc
xiamin <jsdtxm@live.com>
2025-11-06 08:04:06 +0000

    Revert "refactor(serializers): update Field describe"
    This reverts commit a539eae06e9cc9ab02484eaf259763bec427192f.

--- fastapp/serializers/fields/__init__.py
+++ fastapp/serializers/fields/__init__.py
@@ -1,6 +1,5 @@
 import datetime
 from decimal import Decimal
-from functools import partial
 from typing import (
     TYPE_CHECKING,
     Any,
@@ -37,61 +36,7 @@ if TYPE_CHECKING:
             self, instance: "Serializer", owner: Type["Serializer"]
         ) -> VALUE: ...
 else:
-
-    class Field(RawField):
-        def describe(self, serializable: bool) -> dict:
-            def _type_name(typ: Type) -> str:
-                if typ.__module__ == "builtins":
-                    return typ.__name__
-                if typ.__module__ == "typing":
-                    return str(typ).replace("typing.", "")
-                return f"{typ.__module__}.{typ.__name__}"
-
-            def type_name(typ: Any) -> Union[str, List[str]]:
-                try:
-                    return typ._meta.full_name
-                except (AttributeError, TypeError):
-                    pass
-                try:
-                    return _type_name(typ)
-                except AttributeError:
-                    try:
-                        return [_type_name(_typ) for _typ in typ]  # pragma: nobranch
-                    except TypeError:
-                        return str(typ)
-
-            def default_name(default: Any) -> Optional[Union[int, float, str, bool]]:
-                if isinstance(default, (int, float, str, bool, type(None))):
-                    return default
-                if isinstance(default, partial):
-                    return str(default)
-                if callable(default):
-                    return f"<function {default.__module__}.{default.__name__}>"
-                return str(default)
-
-            field_type = getattr(self, "related_model", self.field_type)
-            desc = {
-                "name": self.model_field_name,
-                "field_type": self.__class__.__name__
-                if serializable
-                else self.__class__,
-                "db_column": self.source_field or self.model_field_name,
-                "python_type": type_name(field_type) if serializable else field_type,
-                "generated": self.generated,
-                "nullable": self.null,
-                "unique": self.unique,
-                "indexed": self.index or self.unique,
-                "default": default_name(self.default) if serializable else self.default,
-                "description": self.description,
-                "docstring": self.docstring,
-                "constraints": self.constraints,
-            }
-
-            if self.has_db_field:
-                desc["db_field_types"] = self.get_db_field_types()
-
-            return desc
-
+    Field = RawField
 
 class SerializerMixin(Generic[VALUE]):
     default_value = None
@@ -144,9 +89,7 @@ class IntegerField(SerializerMixin[int], models_data_fields.IntegerField):
 class BigIntegerField(SerializerMixin[int], models_data_fields.BigIntegerField):
     default_value = 0
 
-
 # TODO 添加PositiveIntegerField
-
 
 # Float
 class FloatField(SerializerMixin[float], models_data_fields.FloatField):
