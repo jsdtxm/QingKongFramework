204b3ad18804f58d370f95de20a70d1301d1ccd7
xiamin <jsdtxm@live.com>
2026-03-11 15:51:12 +0800

    refactor(mail): remove mail_managers

--- fastapp/core/mail/__init__.py
+++ fastapp/core/mail/__init__.py
@@ -100,23 +100,3 @@ async def mail_admins(
     if html_message:
         mail.attach_alternative(html_message, "text/html")
     await mail.send(fail_silently=fail_silently)
-
-
-async def mail_managers(
-    subject, message, fail_silently=False, connection=None, html_message=None
-):
-    """Send a message to the managers, as defined by the MANAGERS setting."""
-    if not settings.MANAGERS:
-        return
-    if not all(isinstance(a, (list, tuple)) and len(a) == 2 for a in settings.MANAGERS):
-        raise ValueError("The MANAGERS setting must be a list of 2-tuples.")
-    mail = EmailMultiAlternatives(
-        "%s%s" % (settings.EMAIL_SUBJECT_PREFIX, subject),
-        message,
-        settings.SERVER_EMAIL,
-        [a[1] for a in settings.MANAGERS],
-        connection=connection,
-    )
-    if html_message:
-        mail.attach_alternative(html_message, "text/html")
-    await mail.send(fail_silently=fail_silently)
